6b4721f974e5c19a3c46238b1f2193f42c089c83
karthik <karthikprabhu23223@gmail.com>
2024-02-13 16:24:52 +0530

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import Home from "./pages/home";
 import Signup from "./pages/signup";
 import { AuthProvider } from "./context/auth";
+import PublicRoute from "./routes/PublicRoute";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={
+        <PublicRoute>
+          <Signup/>
+        </PublicRoute>} />
       </Routes>
     </AuthProvider>
   );
